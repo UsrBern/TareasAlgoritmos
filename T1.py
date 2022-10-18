3df9ecb318ede706af699dfca42edd4ca1ab54ee
UsrBern <bernardo.pinninghoff@usm.cl>
2022-10-18 13:52:53 -0300

    Fase final, falta arreglar index error l.23

--- T1.py
+++ T1.py
@@ -4,33 +4,40 @@ def removeElem(m,i): # Quita el i-esimo elemento del arreglo m y retorna el arre
     return m
 
 
+# darCambio(M,i,n,resto,monedas): funcion de recursion
+# M: Arreglo con los tipos de monedas
+# n: el cambio a dar
+# i: Largo del arreglo
+# resto: N%M[i], se usa para la recursion
+# monedas: cantidad de monedas requeridas para pagar el cambio n
+
 def darCambio(M, i, n, resto, monedas):
     if resto == 0:
         return monedas
     elif i == 1:
-        return n//M[1]
+        return n//M[0]
     else:
         if n in M:
             return 1
         else:
-            return min(darCambio(removeElem(M,i-1), n, n%M[i], monedas+n//M[i]), darCambio(removeElem(M,i-1), n, n%M[i], monedas+n//M[i]))
+            return min(darCambio(removeElem(M,i-2), n, n%M[i-1], monedas+n//M[i-1]), # el ultimo tipo requiere menos monedas
+                       darCambio(M[:i-1], n, n%M[i-2], monedas+n//M[i-2])) # el penultimo tipo requiere menos monedas
         
 
 
 
 
 
-n = int(input("Por favor ingresar valor n: "))
+n = int(input())
 while n < 0 or type(n) != int:
     n = int(input("Valor invalido ingresado por favor reintentar.\n"))
 
-m = int(input("Por favor ingresar valor m: "))
+m = int(input())
 while type(m) != int:
     m = int(input("Valor invalido ingresado por favor reintentar.\n"))
 
 M = []
 for i in range(0,m):
-    print("Por favor ingresar denominacion de la moneda ",i+1,":")
     M.append(int(input()))
 
-print(darCambio(M, m, n, -1, 0))
+print(darCambio(M, m, n, 1, 0))
